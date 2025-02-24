68d94ebdb6f44c4df48fb9dae6fc6fbd168ec68a
EnemyStand <85677357+EnemyStand@users.noreply.github.com>
2025-02-24 10:47:26 +0100

    Ball gravity is back baby
    Ball now has gravity all the time, and does not become dropped when touching the ground.

--- Assets/Game Elements/Scripts/Ball Behaviour/BallSM.cs
+++ Assets/Game Elements/Scripts/Ball Behaviour/BallSM.cs
@@ -169,7 +169,7 @@ public class BallSM : MonoBehaviour
                 switch (other.gameObject.tag)
                 {
                     case "Floor":
-                        ChangeState(GetComponent<DroppedState>());
+                        // ChangeState(GetComponent<DroppedState>());
                         break;
                     case "Bouncer":
                         if (bounces < maxBounces)
@@ -189,7 +189,7 @@ public class BallSM : MonoBehaviour
                 switch (other.gameObject.tag)
                 {
                     case "Floor":
-                        ChangeState(GetComponent<DroppedState>());
+                        // ChangeState(GetComponent<DroppedState>());
                         break;
                 }
                 break;
