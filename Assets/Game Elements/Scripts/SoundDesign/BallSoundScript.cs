ef829b1eeb2515982769a490340be8db2c5a05eb
EthanAzoulay <etazoulay@gmail.com>
2025-05-05 16:38:57 +0200

    Sound Design upgrade

--- Assets/Game Elements/Scripts/SoundDesign/BallSoundScript.cs
+++ Assets/Game Elements/Scripts/SoundDesign/BallSoundScript.cs
@@ -2,9 +2,19 @@ using UnityEngine;
 
 public class BallSoundScript : MonoBehaviour
 {
-   
-    
-    
+    private BallSM BallScript;
+    private float speedPercent;
+
+    private void OnEnable()
+    {
+        BallScript = GetComponent<BallSM>();
+
+    }
+
+    private void Update()
+    {
+        speedPercent = BallScript.rb.linearVelocity.magnitude / BallScript.maxSpeed;
+    }
     public void PlaySound()
     {
         AudioManager.instance.PlayOneShot(FMODEvents.instance.ClassicBounce_FX, this.transform.position);
